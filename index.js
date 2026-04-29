9f425af278d6671573ef71a75212d1eb7618039d
ppomega <prateekveerbhan405@gmail.com>
2026-04-29 17:52:41 +0530

    u

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express= require("express");
 const app =express();
+const items = require("./orm/inventory");
+const orm = require("mongoose");
+require('dotenv').config()
 
 app.listen(8000,()=>{
     console.log("Server Sarted");
@@ -7,4 +10,15 @@ app.listen(8000,()=>{
 app.get("/",(req,res)=>{
 
     res.send("Hello PP");
+});
+
+
+app.get("/items",async (req,res)=>{
+await orm.connect(process.env.DB);
+console.log("connected");
+const m =await orm.model("Inventory",items);
+const j = new m({"sbs":"dhh"});
+j.save();
+res.send(j);
+
 });
---
+++ orm/inventory.js
@@ -0,0 +1,10 @@
+const orm = require('mongoose');
+
+module.exports= new orm.Schema({
+    name:String,
+    buyingPrice:Number,
+    sellingPrice:Number,
+    mode:Array,
+    category:String,
+    weight:Number
+});
