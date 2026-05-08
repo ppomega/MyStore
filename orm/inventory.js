f316102fbb8a62471029fa87e46a17e1fc8e9625
ppomega <prateekveerbhan405@gmail.com>
2026-05-08 15:15:03 +0530

    feat: update inventory schema to use Map for mode with validation for allowed keys

--- orm/inventory.js
+++ orm/inventory.js
@@ -1,10 +1,53 @@
-const orm = require('mongoose');
+const orm = require("mongoose");
 
-module.exports= new orm.Schema({
-    name:String,
-    buyingPrice:Number,
-    sellingPrice:Number,
-    mode:Array,
-    category:String,
-    weight:String,
+const ALLOWED_MODE_KEYS = [
+  "Loose",
+  "Piece",
+  "Bottle",
+  "Packet",
+  "Ladi",
+  "Set",
+  "Bag",
+  "Katta",
+  "Petti",
+];
+
+const allowedModeKeySet = new Set(ALLOWED_MODE_KEYS);
+
+function getModeKeys(mode) {
+  if (!mode) {
+    return [];
+  }
+
+  if (mode instanceof Map) {
+    return Array.from(mode.keys());
+  }
+
+  return Object.keys(mode);
+}
+
+module.exports = new orm.Schema({
+  name: String,
+  buyingPrice: Number,
+  sellingPrice: Number,
+  mode: {
+    type: Map,
+    of: {
+      type: Number,
+      validate: {
+        validator(value) {
+          return Number.isInteger(value);
+        },
+        message: "Mode quantities must be integers",
+      },
+    },
+    validate: {
+      validator(mode) {
+        return getModeKeys(mode).every((key) => allowedModeKeySet.has(key));
+      },
+      message: `Mode can only contain these keys: ${ALLOWED_MODE_KEYS.join(", ")}`,
+    },
+  },
+  category: String,
+  weight: String,
 });
